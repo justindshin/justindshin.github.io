ee46d9163da55ebd819b42cf17715ae7e271ff4f
Justin Drake Shin <jdshin@brandeis.edu>
2018-08-27 14:10:53 -0400

    minor changes

--- mail.php
+++ mail.php
@@ -7,5 +7,5 @@ $recipient = "jdshin@brandeis.edu";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+echo "Thank You!" . " -" . "<a href='https://justindshin.github.io/#' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
